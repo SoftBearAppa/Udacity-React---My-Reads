341aa6a7c2fa2500c88533ebc11fa85e2736e51d
SoftBearFeet <SoftBearFeet@users.noreply.github.com>
2017-07-18 20:57:18 -0800

    Added function to gather 'books' prop data and generate 'Book' components and append them to the page.

--- src/assets/componets/Shelf.js
+++ src/assets/componets/Shelf.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react'
 
+import Book from './Book'
+
 class Shelf extends Component {
 	constructor(props) {
 		super(props);
+		this.generateBooks = this.generateBooks.bind(this)
+	}
+
+	/* This gets called to generate a 'Book' component and push it into an array to be appened to the page */
+	generateBooks(data) {
+		const arraydata = [];
+		data.map(datas =>
+			arraydata.push(<Book bookTitle={datas.title} bookAuthor={datas.authors} bookShelf={datas.shelf} bookCover={datas.url}/>))
+			return arraydata;
 	}
 
 	render() {
+
+		/* Sets the 'shelfData' prop as a variable for ease of refference */
+		const books = this.props.books;
+
 		return(
-			<section className="bookself">
+			<section className="bookshelf">
 				<h2 className="bookshelf-title">{this.props.headerTitle}</h2>
-				<div classname="bookshelf-books">
+				<div className="bookshelf-books">
 					<ol className="books-grid">
-						{this.props.bookData}
+					{this.generateBooks(this.props.books)}
 					</ol>
 				</div>
 			</section>
