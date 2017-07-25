c392813a56b34d58029a93396c4e9abf638e006b
SoftBearFeet <SoftBearFeet@users.noreply.github.com>
2017-07-24 19:07:39 -0800

    Added props to Shelf to support functionality to move/remove books.

--- src/assets/componets/MainPage.js
+++ src/assets/componets/MainPage.js
@@ -3,14 +3,13 @@ import React, { Component } from 'react'
 import Shelf from './Shelf'
 
 class MainPage extends Component {
-	constructor(props) {
-		super(props);
-	}
 
 	render() {
 
-		/* Sets the 'shelfData' prop as a variable for ease of refference */
+		/* Sets the prop as a variables for ease of refference */
 		const shelfData = this.props.shelfData;
+		const moveBook = this.props.moves;
+		const removeBook = this.props.remove;
 
 		return(
 			<div className="list-books">
@@ -19,9 +18,9 @@ class MainPage extends Component {
 				</div>
 				<div className="list-books-content">
 					<div>
-						<Shelf headerTitle="Currently Reading" books={shelfData[0]}/>
-						<Shelf headerTitle="Want to Read" books={shelfData[1]} />
-						<Shelf headerTitle="Read" books={shelfData[2]}/>
+						<Shelf value="currentlyReadingShelf" headerTitle="Currently Reading" books={shelfData[0]} moves={moveBook} remove={removeBook} />
+						<Shelf value="wantToReadShelf" headerTitle="Want to Read" books={shelfData[1]} moves={moveBook} remove={removeBook} />
+						<Shelf value="readShelf" headerTitle="Read" books={shelfData[2]} moves={moveBook} remove={removeBook} />
 					</div>
 				</div>
 			</div>
