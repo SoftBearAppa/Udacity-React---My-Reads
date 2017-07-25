1b717df55564491c36ecd341a7834d8351cd9282
SoftBearFeet <SoftBearFeet@users.noreply.github.com>
2017-07-24 19:15:46 -0800

    Added 'changeShelf' function. Also added the 'value' of 'select' element to be set to its shelf state.

--- src/assets/componets/BookShelfChanger.js
+++ src/assets/componets/BookShelfChanger.js
@@ -3,17 +3,28 @@ import React, { Component } from 'react'
 class BookShelfChanger extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			shelf: this.props.shelf
+		}
+		this.changeShelf = this.changeShelf.bind(this);
+	}
+
+	changeShelf(e) {
+		if (e.target.value === 'none') {
+			return this.props.remove();
+		}
+		this.props.moves(e.target.value)
 	}
 
 	render() {
 		return(
 			<div className="book-shelf-changer">
-				<select>
+				<select value={this.state.shelf} onChange={this.changeShelf}>
 					<option value="none" disabled>Move to...</option>
-					<option value="currentlyReading">Currently Reading</option>
-					<option value="wantToRead">Want to Read</option>
-					<option value="read">Read</option>
-					<option value="none">None</option>
+					<option value="currentlyReadingShelf">Currently Reading</option>
+					<option value="wantToReadShelf">Want to Read</option>
+					<option value="readShelf">Read</option>
+					<option value="none">Remove</option>
 				</select>
 			</div>
 		)
